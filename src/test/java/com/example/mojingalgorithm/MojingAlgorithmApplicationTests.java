9a79053eafc01f4b645f7af2d16b1ea51be9ffe0
RuiTan <1063932362@qq.com>
2022-05-23 10:04:55 +0800

    修改模型预测结果模块

--- src/test/java/com/example/mojingalgorithm/MojingAlgorithmApplicationTests.java
+++ src/test/java/com/example/mojingalgorithm/MojingAlgorithmApplicationTests.java
@@ -1,13 +1,23 @@
 package com.example.mojingalgorithm;
 
 import org.junit.jupiter.api.Test;
+import org.openjdk.jol.info.ClassLayout;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MojingAlgorithmApplicationTests {
 
+    static class A {
+        int a;
+        int[] b;
+        int[] c = new int[3];
+    }
+
     @Test
     void contextLoads() {
+        A a = new A();
+        String b = "abc";
+        System.out.println(ClassLayout.parseInstance(b).toPrintable());
     }
 
 }
